3d46e368522878abb23195f97f46fde8d60ec3d2
WangYang-bit <wangkevin2023@gmail.com>
2024-10-15 15:13:51 +0800

    Update logger_config.py

--- util/logger_config.py
+++ util/logger_config.py
@@ -3,22 +3,17 @@ import colorlog
 
 
 def setup_logging(log_file_path):
-    # 创建一个格式化器，定义日志的格式
     formatter = logging.Formatter('%(asctime)s :%(levelname)s:[%(threadName)s] %(message)s')
 
 
-    # 创建 logger
     logger = logging.getLogger("logger")
 
-    # 设置全局日志级别
     logger.setLevel(logging.DEBUG)
 
-    # 创建文件处理器
     file_handler = logging.FileHandler(log_file_path, encoding='utf-8')
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
 
-    # 创建颜色控制台处理器
     console_color_handler = colorlog.StreamHandler()
     console_color_handler.setFormatter(colorlog.ColoredFormatter(
         "%(log_color)s%(asctime)s %(levelname)s:[%(threadName)s] %(message)s",
@@ -32,7 +27,6 @@ def setup_logging(log_file_path):
     ))
     console_color_handler.setLevel(logging.INFO)
 
-    # 将处理器添加到 logger
     logger.addHandler(file_handler)
     logger.addHandler(console_color_handler)
 
