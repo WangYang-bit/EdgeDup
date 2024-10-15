634d6209bdea541c3c61dd166484961b32a4bc5a
WangYang-bit <wangkevin2023@gmail.com>
2024-10-15 15:15:18 +0800

    Update request_generator.py

--- util/request_generator.py
+++ util/request_generator.py
@@ -18,14 +18,12 @@ ServerConf = oc.structured(config.ServerConfig)
 
 
 def generate_long_tail_weights(num_data, alpha=1.1):
-    # 从幂律分布中采样权值
 
     x = np.random.zipf(alpha, size=50000)
     weights = []
     for k in np.unique(x):
         count = np.sum(x == k)
         weights.append(count)
-    # 将权值进行归一化，使其总和为1
     weights = np.sort(weights)[::-1]
     weights = weights[0:num_data]
     weights = (weights + 0.0) / np.sum(weights)
@@ -46,7 +44,6 @@ def data_heat_calculate(request_list, output_file='dataHot10.csv'):
     df = pd.DataFrame(dataHot)
     df = df.sort_values(by="heat", ascending=False)
     hot_percentage = sum(df['heat'][0:round(data_num * 0.2)]) / sum(df['heat'])
-    log.info(f'数据请求分布：前20%请求占{hot_percentage}')
     df.to_csv(output_file, index=False, header=True)
     return dataHot
 
@@ -68,13 +65,11 @@ def generate_data_request(request_num, server_list):
 
 
 def generate_data_request_unbalanced(data_num, server_num, output_file='request_10.csv'):
-    # 生成服务器-数据对的请求次数
-    zipf_parameters = np.random.uniform(low=1.2, high=2.0, size=server_num)  # 为每个服务器生成不同的齐夫分布参数
+    zipf_parameters = np.random.uniform(low=1.2, high=2.0, size=server_num) 
     request_num = np.random.uniform(low=750, high=1250, size=server_num)
     requests = np.zeros((server_num, data_num), dtype=float)
 
     for server_id, parameter in enumerate(zipf_parameters):
-        log.info(f'生成服务器{server_id}的请求分布')
         x = []
         while (len(np.unique(x)) < data_num):
             x.extend(np.random.zipf(parameter, size=server_num * data_num))
@@ -82,22 +77,18 @@ def generate_data_request_unbalanced(data_num, server_num, output_file='request_
         for k in np.unique(x):
             count = np.sum(x == k)
             weights.append(count)
-        # 将权值进行归一化，使其总和为1
         weights = np.sort(weights)[::-1]
         weights = weights[0:data_num]
         weights = (weights + 0.0) / np.sum(weights)
         random.shuffle(weights)
         requests[server_id] = weights
 
-    # 构建请求队列
     request_queue = []
     for server_id, server_requests in enumerate(requests):
         for data_id, num_requests in enumerate(server_requests):
             request_queue.extend([(data_id, server_id)] * np.round(num_requests * request_num[server_id]).astype(int))
     random.shuffle(request_queue)
-    # 将请求队列写入CSV文件
     df = pd.DataFrame(request_queue, columns=['data_id', 'server'])
-    log.info(f'生成请求队列，共{len(request_queue)}个请求')
     df.to_csv(output_file, index=False)
     return request_queue
 
@@ -148,39 +139,29 @@ def server_data_init_with_hot(data_file, server_num, output_file='cache_init10.j
 
 
 def generate_cache_data(requests):
-    # 计算每个请求ID的权重（出现次数）
     weight_dict = dict(Counter(requests))
-    # 计算需要选择的请求数量（总类的30%）
     num_to_select = max(1, len(set(weight_dict)) * 30 // 100)
 
-    # 执行加权无放回随机抽样
     selected_requests = set()
     while len(selected_requests) < num_to_select:
-        # 根据当前权重随机选择一个请求ID
         choices = list(weight_dict.keys())
         weights = list(weight_dict.values())
         weights_p = weights / np.sum(weights)
         selected = np.random.choice(choices, p=weights_p)
         # selected = np.random.choice(choices)
-        # 将选择的请求ID添加到结果集中
         selected_requests.add(selected)
-        # 更新权重，将已选择的请求ID权重设置为0
         del weight_dict[selected]
     return list(selected_requests)
 
 
 # generate data analyse function
 def draw_request_plot(request_file):
-    # 读取CSV文件
     df = pd.read_csv(request_file)
 
-    # 统计请求次数的分布
     request_counts = df['data_id'].value_counts()
 
-    # 对请求次数进行降序排序
     sorted_request_counts = request_counts.sort_values(ascending=False)
     index = [i for i in range(len(sorted_request_counts))]
-    # 绘制折线图
     sns.lineplot(x=index, y=sorted_request_counts.values, marker='')
     plt.xlabel('range')
     plt.ylabel('request num')
@@ -190,20 +171,16 @@ def draw_request_plot(request_file):
 
 
 def draw_request_plot_by_server(request_file):
-    # 读取CSV文件
     df = pd.read_csv(request_file)
 
-    # 绘制每个服务器的文件访问数量分布柱状图
     servers = df['server'].unique()
 
     for server in servers:
         server_data = df[df['server'] == server]
 
-        # 统计每个文件的访问数量
         file_counts = server_data['data_id'].value_counts().reset_index()
         file_counts.columns = ['data_id', 'request num']
 
-        # 取前十的文件
         top_files = file_counts.sort_values(by='request num', ascending=False).head(10)
         top_files.reset_index(inplace=True)
 
