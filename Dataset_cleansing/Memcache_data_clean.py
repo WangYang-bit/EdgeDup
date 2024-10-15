2ce6ff2a7ec838125d4ac1ca193af9bd131550e5
WangYang-bit <wangkevin2023@gmail.com>
2024-10-15 15:20:55 +0800

    Update Memcache_data_clean.py

--- Dataset_cleansing/Memcache_data_clean.py
+++ Dataset_cleansing/Memcache_data_clean.py
@@ -7,38 +7,25 @@ import numpy as np
 
 
 def split_data_set(data_file, output_folder, batch_size=15):
-    # 确保输出文件夹存在
     os.makedirs(output_folder, exist_ok=True)
-    # 列名
     column_names = ['Timestamp', 'data_id', '', 'size', 'client', 'operation', 'TTL']
-    # 初始化变量
-    # 用于存储当前正在处理的timestamp
+
     current_timestamps = set()
-    # 记录当前CSV文件的行数
     current_count = 0
-    # 记录当前是第几个CSV文件
     file_index = 1
 
     with open(data_file, 'r', encoding='utf-8') as large_file:
-        # 创建一个CSV文件用于写入
         output_file = f"{output_folder}/output_batch_{file_index}.csv"
         csv_file = open(output_file, 'w', encoding='utf-8', newline='')
         csv_writer = csv.writer(csv_file)
-        # 写入列名
         csv_writer.writerow(column_names)
 
-        # 逐行读取大型文本文件
         for line in large_file:
-            # 分割行以获取数据
             data = line.strip().split(',')
-            # 检查timestamp是否是新的
             if data[0] not in current_timestamps:
-                # 如果已经有10个不同的timestamp，则开始新的CSV文件
                 if len(current_timestamps) == batch_size:
-                    # 重置timestamp集合和行数计数器
                     current_timestamps.clear()
                     current_count = 0
-                    # 关闭当前CSV文件并打开一个新的CSV文件
                     csv_file.close()
                     file_index += 1
                     output_file = f"{output_folder}/output_batch_{file_index}.csv"
@@ -46,20 +33,14 @@ def split_data_set(data_file, output_folder, batch_size=15):
                     csv_writer = csv.writer(csv_file)
                     csv_writer.writerow(column_names)
 
-                # 添加新的timestamp到集合中
                 current_timestamps.add(data[0])
 
-            # 写入数据到CSV文件
             csv_writer.writerow(data)
             current_count += 1
 
-        # 关闭最后一个CSV文件
         csv_file.close()
 
-    print('所有数据已处理完毕。')
 
-
-# 为client随机分配一个0到50之间的数字
 def randomize_client(server_num):
     return np.random.randint(0, server_num)
 
@@ -104,23 +85,18 @@ def generate_normalized_weights_zipf(n, a=2.1):
 
     return normalized_weights
 
-# 处理单个CSV文件
 def data_set_mapping(file_folder,server_num,output_folder):
-    # 映射data_id到连续的数字
     data_id_mapping = {}
     client_mapping = {}
     data_id_server = {}
     current_index = 0
     file_count = 0
-    # 遍历文件夹中的所有CSV文件并处理它们
     os.makedirs(output_folder, exist_ok=True)
     server_list = list(range(server_num))
     for filename in os.listdir(file_folder):
         if filename.endswith('.csv'):
             file_path = os.path.join(file_folder, filename)
-            # 读取CSV文件
             df = pd.read_csv(file_path)
-            # 映射data_id
             for idx, value in enumerate(df['data_id']):
                 if value not in data_id_mapping:
                     data_id_mapping[value] = current_index
@@ -131,25 +107,20 @@ def data_set_mapping(file_folder,server_num,output_folder):
                 # df.at[idx, 'server'] = np.random.choice(range(server_num), p=data_id_server[value])
                 df.at[idx, 'server'] = np.random.choice(data_id_server[value])
 
-            # # 随机映射client到0-50并重命名为server
             # for idx, value in enumerate(df['client']):
             #     if value not in client_mapping:
             #         data_id_mapping[value] = np.random.randint(0, server_num)
             #     df.at[idx, 'client'] = data_id_mapping[value]
             # df.rename(columns={'client': 'server'}, inplace=True)
 
-
-            # 保存修改后的文件
             output_filename = f"timestamp_{file_count}.csv"
             output_filename_path = os.path.join(output_folder, output_filename)
             df.to_csv(output_filename_path, index=False)
             file_count += 1
-            print(f'已处理{file_count}个文件。')
 
 
-# # 设置输入文件路径和输出文件夹路径
-input_file_path = 'D:\MemcacheTrace\source'
-output_folder_path = 'D:\MemcacheTrace\\trace2\_10server_trace'
+input_file_path = '.\MemcacheTrace\source'
+output_folder_path = '.\MemcacheTrace\\trace2\_10server_trace'
 # split_data_set(input_file_path, output_folder_path,10)
 
 data_set_mapping(input_file_path, 10, output_folder_path)
